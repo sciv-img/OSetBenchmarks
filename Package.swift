d01b81e49b0d27780a7f39dcadf6b7a9401619aa
KenjiTakahashi <wozniakk@gmail.com>
2017-10-28 02:28:32 +0200

    Benchmarks for Bradhilton
    Also update swift-tools-version to 4.0, so we can use `.revision`.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,19 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "OSetBenchmarks",
     dependencies: [
-        .Package(url: "https://github.com/nvzqz/RandomKit", Version(5, 2, 3)),
-        .Package(url: "../OSet", Version(0, 3, 5))
+        .package(url: "https://github.com/nvzqz/RandomKit", .exact("5.2.3")),
+        .package(url: "../OSet", .exact("0.3.5")),
+        .package(url: "https://github.com/bradhilton/OrderedSet", .revision("027fb128af31e8951cbbde9c377b18c90d8189cc")),
+    ],
+    targets: [
+        .target(name: "OSetBenchmarks", path: "Sources"),
+        .testTarget(
+            name: "OSetBenchmarksTests",
+            dependencies: ["RandomKit", "OSet", "OrderedSet", "OSetBenchmarks"]
+        ),
     ]
 )
---
+++ Tests/OSetBenchmarksTests/Bradhilton.swift
@@ -0,0 +1,103 @@
+import XCTest
+import OrderedSet
+
+class BradhiltonBenchmarks: XCTestCase {
+    func testContains() {
+        let (input1_1, _, _) = BenchmarksInputs.inputsBig
+
+        let orderedset1 = OrderedSet(input1_1)
+
+        self.measure {
+            _ = orderedset1.contains(input1_1[102400])
+        }
+    }
+
+    func testInsert() {
+        let (input1_1, _, _) = BenchmarksInputs.inputsBig
+
+        let orderedset1 = OrderedSet(input1_1)
+
+        self.measure {
+            var o1 = orderedset1
+            o1 += ["SOMEOTHERSTRINGTEST"]
+        }
+    }
+
+    func testRemove() {
+        let (input1_1, _, _) = BenchmarksInputs.inputsBig
+
+        let orderedset1 = OrderedSet(input1_1)
+
+        self.measure {
+            var o1 = orderedset1
+            _ = o1.remove(input1_1[102400])
+        }
+    }
+
+    func testUpdate() {
+        let (input1_1, _, _) = BenchmarksInputs.inputsBig
+
+        let orderedset1 = OrderedSet(input1_1)
+
+        self.measure {
+            var o1 = orderedset1
+            o1 += ["SOMEOTHERSTRINGTEST"]
+        }
+    }
+
+    func testUnion() {
+        let (input1_1, input1_2, _) = BenchmarksInputs.inputsSmall
+
+        let orderedset1 = OrderedSet(input1_1)
+        let orderedset2 = OrderedSet(input1_2)
+
+        self.measure {
+            _ = orderedset1.union(orderedset2)
+        }
+    }
+
+    func testIntersection() {
+        let (input1_1, input1_2, input2) = BenchmarksInputs.inputsMedium
+
+        let orderedset1 = OrderedSet(input1_1 + input1_2 + input2)
+        let orderedset2 = OrderedSet(input1_2)
+
+        self.measure {
+            _ = orderedset1.intersection(orderedset2)
+        }
+    }
+
+    func testSymmetricDifference() {
+        let (input1_1, input1_2, input2) = BenchmarksInputs.inputsMedium
+
+        let orderedset1 = OrderedSet(input1_1 + input1_2)
+        let orderedset2 = OrderedSet(input1_2 + input2)
+
+        self.measure {
+            _ = orderedset1.symmetricDifference(orderedset2)
+        }
+    }
+
+    func testSubtracting() {
+        let (input1_1, input1_2, input2) = BenchmarksInputs.inputsMedium
+
+        let orderedset1 = OrderedSet(input1_1 + input1_2 + input2)
+        let orderedset2 = OrderedSet(input1_2)
+
+        self.measure {
+            _ = orderedset1.subtracting(orderedset2)
+        }
+    }
+
+    func testIteration() {
+        let (input1_1, input1_2, input2) = BenchmarksInputs.inputsMedium
+
+        let orderedset1 = OrderedSet(input1_1 + input1_2 + input2)
+
+        self.measure {
+            for e in orderedset1 {
+                _ = e
+            }
+        }
+    }
+}

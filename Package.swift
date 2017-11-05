0026fa32e32e41e5b816afc3f03b51b4ee6e9975
KenjiTakahashi <wozniakk@gmail.com>
2017-11-05 04:28:11 +0100

    Use upstream OSet URL

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "OSetBenchmarks",
     dependencies: [
         .package(url: "https://github.com/nvzqz/RandomKit", .exact("5.2.3")),
-        .package(url: "../OSet", .exact("0.3.5")),
+        .package(url: "https://github.com/sciv-img/OSet", .exact("0.4.0")),
         .package(url: "https://github.com/bradhilton/OrderedSet", .revision("027fb128af31e8951cbbde9c377b18c90d8189cc")),
     ],
     targets: [
